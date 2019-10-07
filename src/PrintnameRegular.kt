59f2a60bd6c6b9929921fa085f167b675887cc09
arccassin <arccassin@gmail.com>
2019-10-07 22:09:53 +0300

    add check index

--- src/PrintnameRegular.kt
+++ src/PrintnameRegular.kt
@@ -6,6 +6,10 @@ fun main(){
     println("Введите ФИО через пробел")
     val strings = readLine()!!.trim().split(' ')
 
+    if (strings.size != 3){
+        println("Неверный формат записи")
+        return
+    }
     val family = strings.get(0)
     val name = strings.get(1)
     val lastname = strings.get(2)
